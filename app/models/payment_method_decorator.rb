00652e066895876c06856fd428b7fbd36b53d6d3
Steven Barragan <me@steven.mx>
2014-01-28 01:20:11 -0600

    Add attached file info to Paymentmethod model

---
+++ app/models/payment_method_decorator.rb
@@ -0,0 +1,11 @@
+Spree::PaymentMethod.class_eval do
+  attr_accessible :icon
+
+  has_attached_file :icon,
+    styles: { mini: '32x32>', normal: '128x128>' },
+    default_style: :mini,
+    convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
+
+  include Spree::Core::S3Support
+  supports_s3 :icon
+end
