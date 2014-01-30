cbe4c319372492519b33282cdfc889213d7cb914
Steven Barragan <steven.barragan@crowdint.com>
2014-01-30 13:47:47 -0600

    Add aws configuration to payemt method image

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -5,6 +5,8 @@ module Spree
     has_attached_file :icon,
       styles: { mini: '32x32>', normal: '128x128>' },
       default_style: :mini,
+      url: '/spree/payment_methods/:id/:style/:basename.:extension',
+      path: ':rails_root/public/spree/payment_methods/:id/:style/:basename.:extension',
       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
     include Spree::Core::S3Support
