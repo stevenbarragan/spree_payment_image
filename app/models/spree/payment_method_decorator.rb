ad3a641626d726daef26ac18dcf23faaf2b29ab9
Steven Barragan <steven.barragan@crowdint.com>
2014-01-31 13:08:33 -0600

    Modify attachment payment_method styles

--- app/models/spree/payment_method_decorator.rb
+++ app/models/spree/payment_method_decorator.rb
@@ -3,7 +3,7 @@ module Spree
     attr_accessible :icon
 
     has_attached_file :icon,
-      styles: { mini: '32x32>', normal: '128x128>' },
+      styles: { mini: 'x32', normal: 'x128' },
       default_style: :mini,
       url: '/spree/payment_methods/:id/:style/:basename.:extension',
       path: ':rails_root/public/spree/payment_methods/:id/:style/:basename.:extension',
